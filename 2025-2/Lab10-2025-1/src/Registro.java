43a97d1fb79edfeaa90591e959fceef761b62bab
erichuizapucp <eric.huiza@pucp.edu.pe>
2025-11-25 09:04:41 -0500

    solución del lab10 2025-1

---
+++ 2025-2/Lab10-2025-1/src/Registro.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author erichuiza
+ */
+public abstract class Registro {
+    public abstract boolean cargarDatos(Scanner scanner);
+    public abstract void imprimir();
+}
